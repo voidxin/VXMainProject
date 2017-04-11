edbabc4419ce5f2cd9573df39d23af82267ad0a8
voidxin <zhangxin409999@163.com>
2017-04-11 15:40:00 +0800

    Initial Commit

---
+++ VXMainProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VXMainProject
+//
+//  Created by voidxin on 17/4/11.
+//  Copyright © 2017年 voidxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VXMainProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VXMainProject
+//
+//  Created by voidxin on 17/4/11.
+//  Copyright © 2017年 voidxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
